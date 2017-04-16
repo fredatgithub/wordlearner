d74b64b64e20a8c0442c09e0a3d42f3694510c07
Alexander Troup <alexander.troup@theappbusiness.com>
2017-04-16 23:43:33 +0100

    allow cross origin requests

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -29,6 +29,13 @@ namespace MvcApp
         {
             // Add framework services.
             services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", 
+                    builder =>{
+                        builder.AllowAnyOrigin();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
